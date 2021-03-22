51877cde7571cedcb39f9fbb4a513d75a25927dc
etrinhh <etrinhh@gmail.com>
2021-03-21 18:16:53 -0600

    create 404 and add dependencies

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-// initiate new project, import express
+// initiate new project, import express and dotenv
 const path = require("path");
 const express = require("express");
 const app = express();
+app.set("view engine", "ejs");
+require("dotenv").config();
+const index = require("./routes");
 
 //use project in public folder
 app.use(express.static(path.join(__dirname, "public")));
 
+//routes
+app.use("/", index);
+
 //create 404 page
 app.use(function (request, response) {
   response.status(404);
-  response.sendFile(__dirname, "/404.html");
+  response.sendFile(__dirname + "/public/404.html");
 });
 
 //server port, use 3000 when process.env not found
