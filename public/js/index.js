70b0f2d3938ae79becf217a93aa270427659c245
etrinhh <etrinhh@gmail.com>
2021-03-21 18:59:12 -0600

    added fetch and error

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,30 @@
 "use strict";
 
-// select gallery, assign as const
-const gallery = document.querySelector(".gallery");
+fetch("api/v0/gallery")
+  .then(function (response) {
+    if (!response.ok) {
+      throw new Error("This is an error");
+    }
+    return response.json();
+  })
+  .then(function (data) {
+    //code from a3
+    // select gallery, assign as const
+    const gallery = document.querySelector(".gallery");
 
-//create output string for html
-let output = "";
+    //create output string for html
+    let output = "";
 
-//loop through each image and string together the html with object keys
-pictures.forEach(function (image) {
-  output += `<figure class="gallery-item">
-  <img src="./assets/images/${image.path}.jpg" alt="${image.title}" style="max-width: ${image.width}px; max-height: ${image.height}px;" />
+    //loop through each image and string together the html with object keys
+    pictures.forEach(function (image) {
+      output += `<figure class="gallery-item">
+  <img src="./public/images/${image.path}.jpg" alt="${image.title}" style="max-width: ${image.width}px; max-height: ${image.height}px;" />
   <figcaption>${image.description}</figcaption>
   </figure>`;
 
-  gallery.innerHTML = output;
-});
-
-/*
-just to reference my old code from gallery assignment 
-              <figure class="gallery-item">
-                <img src="./Assets/Images/happy-gallery.jpg" alt="Happy dog" />
-                <figcaption>Livin my best life</figcaption>
-              </figure>
-*/
+      gallery.innerHTML = output;
+    });
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
